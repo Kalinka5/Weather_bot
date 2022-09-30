c55d33b1ad3044331e2d2b2b792c1aef982cf5e7
Kalinka5 <dania003d@gmail.com>
2022-09-30 18:52:07 +0300

    New version5!

--- handlers/client.py
+++ handlers/client.py
@@ -11,11 +11,11 @@ async def command_start(message: types.Message):
 
 # @dp.message_handler(content_types=['text'])
 async def handle_text(message: types.Message, state: FSMContext):
+    resp_msg = ''
     async with state.proxy() as data:
         if message.text == "🌡️ Temperature":
             weather = data['city']
             celsius = round((weather.current.temperature - 32) / 1.8)
-            resp_msg = ''
 
             resp_msg += f'{weather.nearest_area.name}; {weather.nearest_area.country}\n'
             resp_msg += f'Current temperature: {celsius}°\n'
@@ -59,25 +59,35 @@ async def handle_text(message: types.Message, state: FSMContext):
                             f'\nTemperature will be from {t_lowest} to {t_highest}°C\n' \
                             f'Description: {descriptions}\n\n'
                 # resp_msg += f'{forecast.date:%a}: {t}, {adescriptions}. {emoji}'
+        else:
+            weather = await client.get(message.text)
+            '''for forecast in weather.forecasts:
+                forecast.date.weekday()
+                for hourly in forecast.hourly:
+                    hourly.time'''
+            async with state.proxy() as data:
+                data['city'] = weather
+
+            await message.answer(f'Wow, cool city. Please choose what you need.', reply_markup=markups.mainMenu)
 
         await message.answer(resp_msg)
         await state.finish()
 
 
 # @dp.message_handler()
-async def process_city(message: types.Message, state: FSMContext):
-    weather = await client.get(message.text)
-    '''for forecast in weather.forecasts:
+'''async def process_city(message: types.Message, state: FSMContext):
+    weather = await client.get(message.text)'''
+'''for forecast in weather.forecasts:
         forecast.date.weekday()
         for hourly in forecast.hourly:
             hourly.time'''
-    async with state.proxy() as data:
+'''async with state.proxy() as data:
         data['city'] = weather
 
-    await message.answer(f'Wow, cool city. Please choose what you need.', reply_markup=markups.mainMenu)
+    await message.answer(f'Wow, cool city. Please choose what you need.', reply_markup=markups.mainMenu)'''
 
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=["start", "help"])
     dp.register_message_handler(handle_text)  # , content_types=['text'])
-    dp.register_message_handler(process_city)
+    # dp.register_message_handler(process_city)
